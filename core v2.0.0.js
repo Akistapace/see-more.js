3fd34f862a764908be65b69b83c5c5869c333b3f
Fernando <fernando.akistapace@gmail.com>
2021-06-01 22:26:26 -0300

    Organizado o css -  core.css

--- core v2.0.0.js
+++ core v2.0.0.js
@@ -7,72 +7,70 @@ function see(...args) {
 
     var selector = document.querySelectorAll(`${object[0].el}`)
 
-
     for (var i = 0; i < selector.length; i++) {
         if (selector[i].innerHTML.length > object[0].maxCharVerify) {
-            let selectorContent = selector[i].textContent;
-            let txtShowed = selectorContent.substr(0, object[0].maxCharShow);
-            let txtHidden = selectorContent.substr(object[0].maxCharShow, selector[i].innerHTML.lenght);
+            let selectorContent = selector[i].textContent,
+                txtShowed = selectorContent.substr(0, object[0].maxCharShow),
+                txtHidden = selectorContent.substr(object[0].maxCharShow, selector[i].innerHTML.lenght),
+                classShow = `${object[0].classBntShow.replace(/[.]/g ,'')}`,
+                classHide = `${object[0].classBntHide.replace(/[.]/g ,'')}`,
 
-            // COMṔONENTES TEMPLATES 
-            let templateShowText = `<span>${txtShowed}</span>`
-            let templatehideText = `<span class="minus-text">${txtHidden}</span>`
-            let templateBtnShow = `<button type="button" class="btn-show ${object[0].type || ''} show ${object[0].classBntShow.replace(/[.]/g ,'')}">${object[0].showTxt ||'See more'}</button>`
-            let templateBtnHide = `<button type="button" class="btn-hide ${object[0].type || ''} hide ${object[0].classBntHide.replace(/[.]/g ,'')}">${object[0].hideTxt ||'See less'}</button>`
-            let templateBtnShowDefault = `<span class="${object[0].type || ''} default-show ${object[0].classBntShow.replace(/[.]/g ,'')}">...(${object[0].showTxt ||'See more'})</span>`
-            let templateBtnHideDefault = `<span class="${object[0].type || ''} default-hide ${object[0].classBntHide.replace(/[.]/g ,'')}">(${object[0].hideTxt ||'See more'})</span>`
-            let templateMinusText = `
-            <span class="minus-text">
-                ${templatehideText} 
-                ${templateBtnHide}
-            </span>`;
 
-            let megaElipsis = `<span class="mega-elipse"></span>`
+                // Comṕnentes 
+                component_ShowText = `<span>${txtShowed}</span>`,
+                component_hideText = `<span class="sm_minus-text">${txtHidden}</span>`,
+                component_BtnShow = `<button type="button" class="sm_btn_${object[0].type} sm_show ${classShow}">${object[0].showTxt ||'See more'}</button>`,
+                component_BtnHide = `<button type="button" class="sm_btn_${object[0].type} sm_hide ${classHide}">${object[0].hideTxt ||'See less'}</button>`,
+                component_BtnShowDefault = `<span class="sm_btn_${object[0].type || 'default'} sm_show  ${classShow}">...(${object[0].showTxt || 'See more'})</span>`,
+                component_BtnHideDefault = `<span class="sm_btn_${object[0].type || 'default'}  sm_hide  ${classHide}">(${object[0].hideTxt || 'See less'})</span>`,
+                component_megaElipsis = `<span class="sm_mega-elipse"></span>`,
+                component_MinusText = `<span class="sm_minus-text">${component_hideText} ${component_BtnHide}</span>`;
+
 
             if (object[0].type === 'outline') {
                 selector[i].innerHTML = `
-                ${templateShowText}
-                <span class="see_more-content active ${object[0].type}">
-                    ${templateBtnShow}  
-                    ${templateMinusText}
+                ${component_ShowText}
+                <span class="sm_content sm_active ${object[0].type}">
+                    ${component_BtnShow}  
+                    ${component_MinusText}
                 </span>
-            `;
+                `;
             } else if (object[0].type === 'float') {
-                selector[i].className += ' see_more__float active'
+                selector[i].className += ' sm_float sm_active'
                 selector[i].innerHTML = `
-                ${templateShowText}
-                <span class="see_more-content">
-                    ${templatehideText}             
+                ${component_ShowText}
+                <span class="sm_content">
+                    ${component_hideText}             
                 </span>
-                <span class="container_btn fade">
-                    ${templateBtnShow}  
-                    ${templateBtnHide}
+                <span class="sm_container_btn sm_fade">
+                    ${component_BtnShow}  
+                    ${component_BtnHide}
                 </span>
-            `;
+                `;
 
             } else if (object[0].type === 'mega-elipsis') {
-                selector[i].className += ' see_more__megaelipsis active'
+                selector[i].className += ' sm_mega-elipsis sm_active'
                 selector[i].innerHTML = `
-                ${templateShowText}
-                <span class="see_more-content">
-                    ${templatehideText}    
+                ${component_ShowText}
+                <span class="sm_content">
+                    ${component_hideText}    
                 </span>
-                <div class="mega-elipsis-container fade">         
-                    <div class="flex-mega-elipsis">
-                        ${megaElipsis}
-                        ${megaElipsis}
-                        ${megaElipsis}
+                <div class="sm_mega-elipsis-container sm_fade">         
+                    <div class="sm_flex-mega-elipsis">
+                        ${component_megaElipsis}
+                        ${component_megaElipsis}
+                        ${component_megaElipsis}
                     </div>
                 </div>
                 `;
-            } else { //* DEFAULT E OUTLINE
+            } else { //* Default
                 selector[i].innerHTML = `
-                ${templateShowText}
-                <span class="see_more-content active ${object[0].type}">
-                    ${templateBtnShowDefault} 
+                ${component_ShowText}
+                <span class="sm_content sm_active ${object[0].type || 'default'}">
+                    ${component_BtnShowDefault} 
                     <span class="minus-text">
-                        ${templatehideText} 
-                        ${templateBtnHideDefault}
+                        ${component_hideText} 
+                        ${component_BtnHideDefault}
                     </span>
                 </span>
             `;
@@ -82,44 +80,46 @@ function see(...args) {
     }
 
 
-    function getButtonsFloat() {
-        let see_More__btn = document.querySelectorAll(`${object[0].classBntShow}`);
-        let see_Less__btn = document.querySelectorAll(`${object[0].classBntHide}`);
-        see_More__btn.forEach(el => {
-            el.addEventListener('click', function() {
-                this.parentElement.parentElement.classList.remove('active')
-                console.log(this.parentElement.parentElement);
-            });
-        });
-        see_Less__btn.forEach(el => {
-            el.addEventListener('click', function() {
-                this.parentElement.parentElement.classList.add('active')
-                console.log(this.parentElement.parentElement);
-            });
-        });
-    }
+
 
     function getButtons() {
         let see_More__btn = document.querySelectorAll(`${object[0].classBntShow}`);
         let see_Less__btn = document.querySelectorAll(`${object[0].classBntHide}`);
         see_More__btn.forEach(el => {
             el.addEventListener('click', function() {
-                this.parentElement.classList.remove('active')
+                this.parentElement.classList.remove('sm_active')
             });
         });
 
         see_Less__btn.forEach(el => {
             el.addEventListener('click', function() {
-                this.parentElement.parentElement.classList.add('active')
+                this.parentElement.parentElement.classList.add('sm_active')
             });
         });
     }
 
-    function getButtonsMegaElipsis() {
-        let btnMegaElipsis = document.querySelectorAll('.mega-elipsis-container');
-        btnMegaElipsis.forEach(el => {
+    function getButtonsFloat() {
+        let see_More__btn = document.querySelectorAll(`${object[0].classBntShow}`);
+        let see_Less__btn = document.querySelectorAll(`${object[0].classBntHide}`);
+        see_More__btn.forEach(el => {
             el.addEventListener('click', function() {
-                this.parentElement.classList.toggle('active')
+                this.parentElement.parentElement.classList.remove('sm_active')
+                console.log(this.parentElement.parentElement);
+            });
+        });
+        see_Less__btn.forEach(el => {
+            el.addEventListener('click', function() {
+                this.parentElement.parentElement.classList.add('sm_active')
+                console.log(this.parentElement.parentElement);
+            });
+        });
+    }
+
+    function getButtonscomponent_megaElipsis() {
+        let btncomponent_megaElipsis = document.querySelectorAll('.sm_mega-elipsis-container');
+        btncomponent_megaElipsis.forEach(el => {
+            el.addEventListener('click', function() {
+                this.parentElement.classList.toggle('sm_active')
                 console.log(this.parentElement);
             });
         });
@@ -128,7 +128,7 @@ function see(...args) {
     if (object[0].type === 'float') {
         getButtonsFloat()
     } else if (object[0].type === 'mega-elipsis') {
-        getButtonsMegaElipsis()
+        getButtonscomponent_megaElipsis()
     } else { //* DEFAULT E OUTLINE
         getButtons()
     }
@@ -136,14 +136,10 @@ function see(...args) {
 
 var teste1 = new see({
     el: '.query',
-    showTxt: 'Saber Mais',
-    hideTxt: 'Saber menos',
     maxCharVerify: 10,
     maxCharShow: 251,
     classBntShow: '.btn-show1',
     classBntHide: '.btn-hide1',
-    type: 'default',
-    elipsis: true,
 })
 
 var teste2 = new see({
